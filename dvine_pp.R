41b21e8b2ecd809b2c35f183acb0e1ce1431ea08
David Jobst <31279210+jobstdavid@users.noreply.github.com>
2022-12-19 12:36:41 +0100

    Update dvine_pp.R

--- dvine_pp.R
+++ dvine_pp.R
@@ -1,26 +1,27 @@
-# select library
+# select library (path may needs to be adapted!)
 .libPaths("/home/jobst/miniconda3/envs/r_env/lib/R/library") 
 
 # load packages
 library(doMC)
 library(lubridate)
 library(vinereg)
-library(rvinecopulib)
 
-# load data
+# load data (paths may need to be adapted!)
 load("/home/jobst/benchmark/benchmark_t2m_train_ext.Rdata")
 load("/home/jobst/benchmark/benchmark_t2m_test_ext.Rdata")
 
-# select parameters for cluster
+# select parameters for cluster (parameters may need to be adapted!)
 s <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")) # get array number from batch script 
 cores <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")) # get cores from batch script
 registerDoMC(cores = cores)
 
 
 # D-vine copula postprocessing function with weekly training period
-dvine_pp <- function(tdata, vdata, 
-                     tresponse, tpredictors,
-                     vresponse, vpredictors,
+dvine_pp <- function(tdata, 
+                     vdata, 
+                     tresponse, 
+                     tpredictors,
+                     vpredictors,
                      family_set = "all",
                      selcrit = "bic", 
                      order = NA,
@@ -31,11 +32,39 @@ dvine_pp <- function(tdata, vdata,
                      nweeks,
                      cores = 1){
   
+  ###-----------------------------------------------------------------------------
+  ### Input
+  # tdata .............. training  (data.frame)
+  # vdata .............. validation data (data.frame)
+  # tresponse .......... response variable for the training data (character)
+  # tpredictors ........ predictor variables for the training data (character)
+  # vpredictors ........ predictor variables for the validation/test data (character)
+  # family_set ......... copula families (character)
+  # selcrit ............ predictor variable selection criterion (character)
+  # order .............. predictor variable order (character, logical)
+  # location ........... location/station-id (character)
+  # leadtime ........... leadtime (integer)
+  # ffd ................ first forecast date (character)
+  # lfd ................ last forecast date (character)
+  # nweeks ............. see repo documentation (integer)
+  # cores .............. number of cores for multi-core usage (integer)
+  ###-----------------------------------------------------------------------------
+  ### Output (list)
+  # tquantiles ......... predicted quantiles for the training data (data.frame)
+  # vquantiles ......... predicted quantiles for the validation/test data (data.frame)
+  # runtime.train ...... training time for the model (vector)
+  # runtime.pp ......... time for producing the postprocessed ensemble forecasts (vector)
+  # tcrps .............. CRPS in the training data (data.frame)
+  ###-----------------------------------------------------------------------------
+  
+  # forecast days
   days <- seq(as.Date(ffd), as.Date(lfd), by = "day")
+  # weeks belonging to the forecast days
   w <- week(days)
-  # evaluate last day 365/2 = 52, rest 1 with week 52
+  # evaluate last day 365/2 = 52, rest 1 with week 52; set week 53 to 52 to have more data points
   w[w == 53] <- 52
-  unique_w <- unique(w) # weeks, for each a model is fitted
+  # weeks, for each a model is fitted
+  unique_w <- unique(w) 
   
   # get valid_date, response, predictors and weeks for location and leadtime in training data 
   tloc <- tdata[tdata$id == location & tdata$leadtime == leadtime, c("valid_date", tresponse, tpredictors)]
@@ -45,25 +74,28 @@ dvine_pp <- function(tdata, vdata,
   tloc.all <- tloc
   
   # get valid_date, response, predictors and weeks for location and leadtime in test = validation data 
-  vloc <- vdata[vdata$id == location & vdata$leadtime == leadtime, c("valid_date", vresponse, vpredictors)]
+  vloc <- vdata[vdata$id == location & vdata$leadtime == leadtime, c("valid_date", vpredictors)]
   w_vloc <- week(vloc$valid_date)
   w_vloc[w_vloc == 53] <- 52
   vloc <- cbind(w_vloc, vloc)
   
   # initialize output
-  dvine.order <- list()
-  dvine.copulas <- list()
   runtime.train <- runtime.pp <- c()
   tcrps <- data.frame()
   tquantiles <- data.frame()
   vquantiles <- data.frame()
   
+  # formula for model
   formula <- as.formula(paste0(tresponse,  " ~ ."))
   
+  # iterate through each week in unique_w
   for (k in 1:length(unique_w)) {
     
+    # select the weeks for training
     w_choose <- (unique_w[k] + -nweeks:nweeks) %% 52
     w_choose[w_choose == 0] <- 52
+    
+    # determine final training data for the location
     loc <- tloc[tloc$w_tloc %in% w_choose, c(tresponse, tpredictors)]
     loc <- na.omit(loc)
     
@@ -71,6 +103,7 @@ dvine_pp <- function(tdata, vdata,
     if(nrow(loc) >= 2) {
       
       start_tm <- Sys.time()
+      # fit model
       fit <- vinereg(formula = formula,
                      data = loc, 
                      family_set = family_set,
@@ -83,12 +116,7 @@ dvine_pp <- function(tdata, vdata,
       runtime.train <- c(runtime.train,
                          as.numeric(difftime(end_tm, start_tm, units = "mins")))
       
-      # get info of D-vine copula
-      dvine.order[[k]] <- fit$order
-      dvine.copulas[[k]] <- unlist(get_all_families(fit$vine))
-      
-      
-      # validate model by crps and quantiles in training data (in sample)
+      # validate model by CRPS and quantiles in training data (in sample)
       t.loc.dat <-  tloc.all[tloc.all$w_tloc == unique_w[k], ]
       y <- t.loc.dat[, tresponse]
       x <- predict(fit, newdata = t.loc.dat, alpha = 1:51/52, cores = cores)
@@ -96,7 +124,6 @@ dvine_pp <- function(tdata, vdata,
       crps.k <- crps(y = y, x = x)
       tcrps <- rbind(tcrps, data.frame(week = t.loc.dat$w_tloc,
                                        valid_date = t.loc.dat$valid_date,
-                                       nobs = nrow(loc),
                                        crps = crps.k))
       tquantiles <- rbind(tquantiles, data.frame(week = t.loc.dat$w_tloc,
                                                  valid_date = t.loc.dat$valid_date,
@@ -119,14 +146,11 @@ dvine_pp <- function(tdata, vdata,
       
       runtime.train <- c(runtime.train, NaN)
       runtime.pp <- c(runtime.pp, NaN)
-      dvine.order[[k]] <- NaN
-      dvine.copulas[[k]] <- NaN
       
       # training data evaluation
       t.loc.dat <-  tloc.all[tloc.all$w_tloc == unique_w[k], ]
       tcrps <- rbind(tcrps, data.frame(week = t.loc.dat$w_tloc,
                                        valid_date = t.loc.dat$valid_date,
-                                       nobs = nrow(loc),
                                        crps = NaN))
       tquantiles <- rbind(tquantiles, data.frame(week = t.loc.dat$w_tloc,
                                                  valid_date = t.loc.dat$valid_date,
@@ -142,10 +166,9 @@ dvine_pp <- function(tdata, vdata,
     
   }
   
+  # prepare output
   out <- list(tquantiles = tquantiles[order(tquantiles$valid_date), ],
               vquantiles = vquantiles[order(vquantiles$valid_date), ],
-              order = dvine.order,
-              copulas = dvine.copulas,
               runtime.train = runtime.train,
               runtime.pp = runtime.pp,
               tcrps =  tcrps[order(tcrps$valid_date), ])
@@ -154,17 +177,31 @@ dvine_pp <- function(tdata, vdata,
   
 }
 
+
+# function for CRPS calculation
 crps <- function(y, x) {
   
+  ###-----------------------------------------------------------------------------
+  ### Input
+  # y .............. observations (n-dim. vector)
+  # x .............. ensemble forecasts (nxm-dim. data.frame)
+  ###-----------------------------------------------------------------------------
+  ### Output 
+  # CRPS ......... CRPS (n-dim. vector)
+  ###-----------------------------------------------------------------------------
+  
+  # get necessary parameters
   m <- ncol(x)
   i1 <- rep(1:m, times = m)
   i2 <- rep(1:m, each = m)
   
+  # CRPS calculation
   out <- rowSums(abs(x-y))/m - 1/(2*m^2) * rowSums(abs(x[, i1]-x[, i2]))
+  
   return(out)
 }
 
-# D-vine copula postprocessing settings
+# D-vine copula postprocessing setting
 tdata <- benchmark_t2m_train_ext
 vdata <- benchmark_t2m_test_ext
 tresponse <- vresponse <- "observation"
@@ -179,12 +216,14 @@ dates <- unique(vdata[vdata$leadtime == lt, "valid_date"])
 nw <- nweeks[s]
 ids <- unique(tdata$id)
 
-
+# start evaluation for each station for a fixed leadtime
 results.dvine <- foreach(l = 1:229) %dopar% {
   
-  out <- dvine_pp(tdata, vdata,
-                  tresponse, tpredictors, 
-                  vresponse, vpredictors, 
+  out <- dvine_pp(tdata, 
+                  vdata,
+                  tresponse, 
+                  tpredictors, 
+                  vpredictors, 
                   family_set = family_set,
                   selcrit = selcrit, 
                   order = order,                 
@@ -198,8 +237,6 @@ results.dvine <- foreach(l = 1:229) %dopar% {
   
 }
 
+# save output (path may needs to be adapted!)
 nam <- paste0("/home/jobst/benchmark/results/results.dvine.lt", lt, ".nw", nw, ".Rdata")
 save(results.dvine, file = nam)
-
-
-
